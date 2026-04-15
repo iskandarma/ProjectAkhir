53d43581d860c72a924dee3b449e286d9de88f8b
Moh Iskandar Maulana <mohmaulana65@guru.smk.belajar.id>
2026-04-15 08:08:54 +0700

    memindahkan styling di file home.py ke style.css

--- pages/home.py
+++ pages/home.py
@@ -21,16 +21,9 @@ pie_fig = px.pie(df, names="Academic_Status")
 # Membuat card
 def card(title, value):
     return html.Div([
-        html.H4(title, style={'color': '#495057', 'marginBottom': '5px'}),
-        html.H2(value, style={'color': '#212529', 'fontWeight': 'bold'})
-    ], style={
-        'padding': '20px',
-        'borderRadius': '12px',
-        'backgroundColor': '#ecf0f1',
-        'flex': '1',
-        'textAlign': 'center',
-        'boxShadow': '0 2px 4px rgba(0,0,0,0.05)'
-    })
+        html.H4(title),
+        html.H2(value)
+    ], className='kpi-card')
 
 layout = html.Div([
 
@@ -41,21 +34,14 @@ layout = html.Div([
         card("Total Data", total_students),
         card("Avg Score", avg_score),
         card("Pass Rate", f"{pass_rate}%"),
-    ], style={'display': 'flex', 'gap': '20px', 'width': '100%'}),
+    ], className='kpi-container'),
 
     html.Br(),
 
     # Menampilkan chart
     html.Div([
-        dcc.Graph(figure=hist_fig, style={'flex': '3'}),
-        dcc.Graph(figure=pie_fig, style={'flex': '2'}),
-    ], style={
-        'display': 'flex', 
-        'backgroundColor': 'white', 
-        'padding': '20px', 
-        'borderRadius': '12px',
-        'boxShadow': '0 4px 12px rgba(0,0,0,0.08)',
-        'width': '100%'
-    })
+        dcc.Graph(figure=hist_fig, className='graph-large'),
+        dcc.Graph(figure=pie_fig, className='graph-small'),
+    ], className='chart-container')
 
 ])
